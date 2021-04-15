23ed84e57d04d262beb6ddd71c26d6952fb0f3bb
Daniel Amber <52280840+danownsthisspace@users.noreply.github.com>
2021-04-15 07:14:14 +0200

    Update clear-youtube-playlist.js

--- clear-youtube-playlist.js
+++ clear-youtube-playlist.js
@@ -1,8 +1,7 @@
-// This code is heavily dependent on YouTube not changing their UI.... So it will break one day :p 
+// This code is heavily dependent on YouTube not changing their UI.... So it will break one day
 
 setInterval( () => {
   document.querySelector('.style-scope .ytd-playlist-video-list-renderer #button').click()
-  ​
   document.querySelector('#items > ytd-menu-service-item-renderer:nth-child(3) > tp-yt-paper-item').click()
   }, 200 )
   
